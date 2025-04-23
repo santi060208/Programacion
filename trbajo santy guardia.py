e7dc32eb00df6d53b218d2e587322cfd71771b3b
santi060208 <santiagoguardia180@gmail.com>
2025-04-23 09:20:57 -0300

    Add files via upload

---
+++ trbajo santy guardia.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+def main():
+    ventana=tk.Tk()
+    ventana.title("ejercicio 1 santiago guardia")
+    ventana.geometry( "500x400")
+    ventana.configure(bg="blue")
+    ventana.mainloop()                  
+if __name__=="__main__":
+    main()
