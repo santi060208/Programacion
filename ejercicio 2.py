5af5c1d348649cb05b9954496ef45cd8533c3ab5
santi060208 <santiagoguardia180@gmail.com>
2025-04-23 13:18:13 -0300

    Add files via upload

---
+++ ejercicio 2.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+def main():
+    ventana=tk.TK()
+    ventana.title("ejercicio 2")
+    ventana.geometry("700x700)
+    venatana.configure(bg="azul claro")
+
+    etiqueta=tk.label(ventana,next="ejercicio 2", font=("arial",16)
+     etiqueta.pack()
+    if __name__                  
+        
